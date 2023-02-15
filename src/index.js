76e6c12683b1ddad0a673b8e3c346edb38303049
Nik <nikdekker+github@me.com>
2023-02-15 12:16:09 +0100

    initial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<img src=https://nik-dekker.bynder.com/m/4cf3a982fcd62cd2/relative-lego-0.PNG>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
